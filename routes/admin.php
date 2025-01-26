f376778d33d894fb5b332ed7fe9a84e432b9b69a
Mr. Tetteh <danielstay73@gmail.com>
2025-01-26 11:27:24 +0000

    route on nav config

--- routes/admin.php
+++ routes/admin.php
@@ -3,9 +3,14 @@
 use App\Livewire\Admin\UserDashboard;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', \App\Livewire\Admin\Dashboard::class)->name('admin.dashboard');
-    Route::get('buy-data', \App\Livewire\Admin\Data::class)->name('admin.data');
-    Route::get('top-up', \App\Livewire\Admin\TopUps::class)->name('admin.top-up');
-    Route::get('user-dashboard', UserDashboard::class)->name('admin.user-dashboard');
+Route::middleware('auth')->group(function () {
+
+    Route::prefix('admin')->group(function () {
+        Route::get('/dashboard', \App\Livewire\Admin\Dashboard::class)->name('admin.dashboard');
+        Route::get('buy-data', \App\Livewire\Admin\Data::class)->name('admin.data');
+        Route::get('top-up', \App\Livewire\Admin\TopUps::class)->name('admin.top-up');
+        Route::get('user-dashboard', UserDashboard::class)->name('admin.user-dashboard');
+    });
+
+
 });
