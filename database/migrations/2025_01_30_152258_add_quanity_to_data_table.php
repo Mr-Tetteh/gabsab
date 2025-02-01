17548e5c06a41fb163b750d86cee98bd490adbf0
Mr. Tetteh <danielstay73@gmail.com>
2025-02-01 19:05:50 +0000

    update on data

--- database/migrations/2025_01_30_152258_add_quanity_to_data_table.php
+++ database/migrations/2025_01_30_152258_add_quanity_to_data_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('data', function (Blueprint $table) {
-            $table->string('quantity')->after('amount');
+            $table->string('quantity')->after('amount')->nullable();
         });
     }
 
