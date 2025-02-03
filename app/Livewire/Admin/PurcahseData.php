f08019e532004e7df4de025311f80c885605bf16
Mr. Tetteh <danielstay73@gmail.com>
2025-02-03 16:18:18 +0000

    bulk data

--- app/Livewire/Admin/PurcahseData.php
+++ app/Livewire/Admin/PurcahseData.php
@@ -9,6 +9,13 @@ class PurcahseData extends Component
 {
     #[Layout('layout.admin.partials.website-base-admin')]
 
+    public $refrence;
+    public $packages = [];
+    public $number;
+    public $amount;
+    public $quantity = '';
+    public $user_id;
+
     public function render()
     {
         $datas = \App\Models\Data::all();
