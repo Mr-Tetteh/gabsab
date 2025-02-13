e9bcd925563a0ba700a2c0356c897ab9656c6fd0
Mr. Tetteh <danielstay73@gmail.com>
2025-02-13 15:57:23 +0000

    header

--- app/Livewire/Admin/BuyData.php
+++ app/Livewire/Admin/BuyData.php
@@ -37,10 +37,11 @@ class BuyData extends Component
     {
         $this->validate();
         $Voucher = random_int(00000000, 9999999999);
+
         $packageName = match ($this->package) {
-            '1' => 'One Ghana',
+            '1' => 'One Ghana For Your Pocket',
             '2' => 'Ashanti Two',
-            '3' => 'Blue Up',
+            '5' => 'Blue Up',
             default => 'Unknown Package',
         };
 
@@ -56,7 +57,8 @@ class BuyData extends Component
             '233' . substr($this->number, -9),
             'Dear ' . Auth::user()->first_name . ', your data package ' . $packageName . ' has been purchased successfully. ' .
             'Your Voucher pin is ' . $Voucher . '. Happy browsing.'
-        );        session()->flash('message', 'Data Purchased successfully. You will receive an SMS soon with your Voucher Pin');
+        );
+        session()->flash('message', 'Data Purchased successfully. You will receive an SMS soon with your Voucher Pin');
         $this->resetForm();
 
     }
